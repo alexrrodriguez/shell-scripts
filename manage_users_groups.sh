839bb340289164998714824ee3464fbcbdde1197
alex <arodriguez202020@gmail.com>
2022-01-19 16:25:55 -0600

    create manage users/groups shell file and add shell script examples for Unix user administration

---
+++ manage_users_groups.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# root, system, and user accounts
+# four main user admin files are located in /etc/passwd, /etc/shadow, /etc/group, /etc/gshadow
+
+# main user commands are: useradd, usermod, and userdel
+# main group commands are: groupadd, groupmod, and groupdel
+
+# create a group syntax
+# groupadd [ -g GID [-o]] [-r] [-f] groupname
+
+# parameters
+# -g GID - numerical value of the group's ID
+# -o - This option permits to add group with non-unique GID
+# -r - This flag instructs groupadd to add a system account
+# -f - This option causes to just exit wtih success status, if the specified group already exists
+# groupname - Actual group name to be created
+
+# No specified parameters and the system will use default values
+groupadd developers
+
+
+# modify a group
+groupmod -n new_modified_group old_group
+
+# change developer_2 group name to developer
+groupmod -n developer developer_2
+
+# change the groupID (GID)
+groupmod -g 545 developer
+
+
+# delete a group
+groupdel developer
+
+
+# create an account syntax
+# useradd -d homedir -g groupname -m -s shell -u userid accountname
+# following example creates an account tom
+useradd -d /home/tom -g developers -s /bin/ksh tom
+
+# after user is created, can set password using passwd command
+passwd tom20
+
+
+# modify account using the usermod command syntax(same arguments as useradd plust -l to change account name)
+usermod -d /home/thomas -m -l tom thomas
+
+
+# delete an account
+# userdel command can be used to delete existing user, use with caution, can omit -r to keep home directory for backup purposes
+userdel -r thomas
